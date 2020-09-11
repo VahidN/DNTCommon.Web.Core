735f0388e7fcd83b7d9216ac0ebd3d9ca464364a
Vahid Nasiri <vahid_nasiri@yahoo.com>
2020-09-11 14:48:56 +0430

    Update UrlNormalizationServiceTests

--- src/DNTCommon.Web.Core.Tests/UrlNormalizationServiceTests.cs
+++ src/DNTCommon.Web.Core.Tests/UrlNormalizationServiceTests.cs
@@ -9,7 +9,7 @@ namespace DNTCommon.Web.Core.Tests
         [TestMethod]
         public async Task Test1ConvertingTheSchemeAndHostToLowercase()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
             {
                 var url1 = await urlNormalizationService.NormalizeUrlAsync("HTTP://www.Example.com/", findRedirectUrl: false);
                 var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/", findRedirectUrl: false);
@@ -21,181 +21,181 @@ namespace DNTCommon.Web.Core.Tests
         [TestMethod]
         public async Task Test2CapitalizingLettersInEscapeSequences()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a%c2%b1b", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a%C2%B1b", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a%c2%b1b", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a%C2%B1b", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test3DecodingPercentEncodedOctetsOfUnreservedCharacters()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/%7Eusername/", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/~username/", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/%7Eusername/", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/~username/", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test4RemovingTheDefaultPort()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com:80/bar.html", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/bar.html", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com:80/bar.html", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/bar.html", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test5AddingTrailing()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/alice", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/alice/?", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/alice", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/alice/?", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test6RemovingDotSegments()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/../a/b/../c/./d.html", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a/c/d.html", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/../a/b/../c/./d.html", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a/c/d.html", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test7RemovingDirectoryIndex1()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/default.asp", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/default.asp", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test7RemovingDirectoryIndex2()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/default.asp?id=1", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/default.asp?id=1", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/default.asp?id=1", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/default.asp?id=1", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test7RemovingDirectoryIndex3()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a/index.html", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a/", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a/index.html", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/a/", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test8RemovingTheFragment()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/bar.html#section1", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/bar.html", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/bar.html#section1", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/bar.html", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test9LimitingProtocols()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("https://www.example.com/", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("https://www.example.com/", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test10RemovingDuplicateSlashes()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/foo//bar.html", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/foo/bar.html", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/foo//bar.html", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com/foo/bar.html", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test11AddWww()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://example.com/", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://example.com/", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://www.example.com", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test11AddWww2()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://bob.example.com/", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://bob.example.com", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://bob.example.com/", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://bob.example.com", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test12RemoveFeedburnerPart()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("http://site.net/2013/02/firefox-19-released/?utm_source=rss&utm_medium=rss&utm_campaign=firefox-19-released", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("http://site.net/2013/02/firefox-19-released", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("http://site.net/2013/02/firefox-19-released/?utm_source=rss&utm_medium=rss&utm_campaign=firefox-19-released", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("http://site.net/2013/02/firefox-19-released", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
 
         [TestMethod]
         public async Task Test13QueryStrings()
         {
-            await ServiceProvider.RunScopedService<Task, IUrlNormalizationService>(async urlNormalizationService =>
-            {
-                var url1 = await urlNormalizationService.NormalizeUrlAsync("https://www.youtube.com/watch?v=3TcduzoIA64", findRedirectUrl: false);
-                var url2 = await urlNormalizationService.NormalizeUrlAsync("https://www.youtube.com/watch?v=3TcduzoIA64", findRedirectUrl: false);
+            await ServiceProvider.RunScopedServiceAsync<IUrlNormalizationService>(async urlNormalizationService =>
+           {
+               var url1 = await urlNormalizationService.NormalizeUrlAsync("https://www.youtube.com/watch?v=3TcduzoIA64", findRedirectUrl: false);
+               var url2 = await urlNormalizationService.NormalizeUrlAsync("https://www.youtube.com/watch?v=3TcduzoIA64", findRedirectUrl: false);
 
-                Assert.AreEqual(url1, url2);
-            });
+               Assert.AreEqual(url1, url2);
+           });
         }
     }
 }
