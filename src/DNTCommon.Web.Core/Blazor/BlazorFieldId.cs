70e8e6271667077a1a30ee060126373a614b16dd
VahidN <vahid_nasiri@yahoo.com>
2024-02-29 12:35:30 +0330

    Add BlazorFieldId

---
+++ src/DNTCommon.Web.Core/Blazor/BlazorFieldId.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     Uniquely identifies a single field that can be edited.
+///     This may correspond to a property on a model object, or can be any other named value.
+/// </summary>
+public class BlazorFieldId<T>
+{
+    private readonly Expression<Func<T>>? _valueExpression;
+
+    /// <summary>
+    ///     Uniquely identifies a single field that can be edited.
+    ///     This may correspond to a property on a model object, or can be any other named value.
+    /// </summary>
+    public BlazorFieldId(Expression<Func<T>>? valueExpression)
+        => _valueExpression = valueExpression;
+
+    /// <summary>
+    ///     Uniquely identifies a single field that can be edited.
+    ///     This may correspond to a property on a model object, or can be any other named value.
+    /// </summary>
+    public FieldIdentifier FieldIdentifier
+        => _valueExpression is null ? default : FieldIdentifier.Create(_valueExpression);
+
+    /// <summary>
+    ///     Gets the name of the editable field.
+    /// </summary>
+    public string FieldName
+    {
+        get
+        {
+            var fieldName = FieldIdentifier.FieldName;
+
+            return string.IsNullOrWhiteSpace(fieldName) ? Guid.NewGuid().ToString("N") : fieldName;
+        }
+    }
+
+    /// <summary>
+    ///     Signals that the value for the specified field has changed.
+    ///     Signals that some aspect of validation state has changed.
+    /// </summary>
+    public void NotifyFieldChanged(EditContext? editContext)
+    {
+        if (editContext is null)
+        {
+            return;
+        }
+
+        var fi = FieldIdentifier;
+
+        if (string.IsNullOrWhiteSpace(fi.FieldName))
+        {
+            return;
+        }
+
+        editContext.NotifyFieldChanged(fi);
+        editContext.NotifyValidationStateChanged();
+    }
+}
---
+++ src/DNTCommon.Web.Core/Blazor/DisplayNameExtensions.cs
@@ -0,0 +1,56 @@
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     DisplayAttribute extensions
+/// </summary>
+public static class DisplayNameExtensions
+{
+    /// <summary>
+    ///     Gets the DisplayName of the provided expression
+    /// </summary>
+    public static string GetDisplayName<TEntity>(this Expression<Func<TEntity>>? expression)
+    {
+        var memberExp = expression.GetMemberExpression();
+
+        if (memberExp is null)
+        {
+            throw new ArgumentException($"The expression doesn't indicate a valid property. [ {expression} ]",
+                nameof(expression));
+        }
+
+        var displayAttribute = memberExp.Member.GetCustomAttribute<DisplayAttribute>();
+
+        if (displayAttribute is null)
+        {
+            return string.Empty;
+        }
+
+        return displayAttribute.Name ?? memberExp.Member.Name;
+    }
+
+    /// <summary>
+    ///     Represents accessing a field or property.
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <returns></returns>
+    public static MemberExpression? GetMemberExpression(this Expression? expression)
+    {
+        switch (expression)
+        {
+            case MemberExpression memberExpression:
+                return memberExpression;
+            case LambdaExpression lambdaExpression:
+                switch (lambdaExpression.Body)
+                {
+                    case MemberExpression memExpression:
+                        return memExpression;
+                    case UnaryExpression unaryExpression:
+                        return (MemberExpression)unaryExpression.Operand;
+                }
+
+                break;
+        }
+
+        return null;
+    }
+}
