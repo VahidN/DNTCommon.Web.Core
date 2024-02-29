98afb06d32ae84b6a9e76561cc368a11b6e3739a
VahidN <vahid_nasiri@yahoo.com>
2024-02-29 11:29:41 +0330

    Improve ICacheService, Add TimeSpan based expiration options

--- src/DNTCommon.Web.Core/Caching/MemoryCacheService.cs
+++ src/DNTCommon.Web.Core/Caching/MemoryCacheService.cs
@@ -1,43 +1,35 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace DNTCommon.Web.Core;
 
 /// <summary>
-/// Encapsulates IMemoryCache functionality.
+///     Encapsulates IMemoryCache functionality.
 /// </summary>
 public class MemoryCacheService : ICacheService
 {
     private readonly IMemoryCache _memoryCache;
 
     /// <summary>
-    ///  Encapsulates IMemoryCache functionality.
+    ///     Encapsulates IMemoryCache functionality.
     /// </summary>
     public MemoryCacheService(IMemoryCache memoryCache)
-    {
-        _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
-    }
+        => _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
 
     /// <summary>
-    /// Gets the key's value from the cache.
+    ///     Gets the key's value from the cache.
     /// </summary>
     public T? GetValue<T>(string cacheKey)
-    {
-        return _memoryCache.Get<T>(cacheKey);
-    }
+        => _memoryCache.Get<T>(cacheKey);
 
     /// <summary>
-    /// Tries to get the key's value from the cache.
+    ///     Tries to get the key's value from the cache.
     /// </summary>
     public bool TryGetValue<T>(string cacheKey, out T? result)
-    {
-        return _memoryCache.TryGetValue(cacheKey, out result);
-    }
+        => _memoryCache.TryGetValue(cacheKey, out result);
 
     /// <summary>
-    /// Adds a key-value to the cache.
-    /// It will use the factory method to get the value and then inserts it.
+    ///     Adds a key-value to the cache.
+    ///     It will use the factory method to get the value and then inserts it.
     /// </summary>
     public void Add<T>(string cacheKey, Func<T> factory, DateTimeOffset absoluteExpiration, int size = 1)
     {
@@ -54,33 +46,81 @@ public class MemoryCacheService : ICacheService
     }
 
     /// <summary>
-    /// Adds a key-value to the cache.
+    ///     Adds a key-value to the cache.
+    ///     It will use the factory method to get the value and then inserts it.
+    /// </summary>
+    public void Add<T>(string cacheKey, Func<T> factory, TimeSpan absoluteExpirationRelativeToNow, int size = 1)
+    {
+        if (factory == null)
+        {
+            throw new ArgumentNullException(nameof(factory));
+        }
+
+        _memoryCache.Set(cacheKey, factory(), new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow,
+            Size = size // the size limit is the count of entries
+        });
+    }
+
+    /// <summary>
+    ///     Adds a key-value to the cache.
     /// </summary>
     public void Add<T>(string cacheKey, T value, DateTimeOffset absoluteExpiration, int size = 1)
-    {
-        _memoryCache.Set(cacheKey, value, new MemoryCacheEntryOptions
+        => _memoryCache.Set(cacheKey, value, new MemoryCacheEntryOptions
         {
             AbsoluteExpiration = absoluteExpiration,
             Size = size // the size limit is the count of entries
         });
-    }
 
     /// <summary>
-    /// Adds a key-value to the cache.
+    ///     Adds a key-value to the cache.
+    /// </summary>
+    public void Add<T>(string cacheKey, T value, TimeSpan absoluteExpirationRelativeToNow, int size = 1)
+        => _memoryCache.Set(cacheKey, value, new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow,
+            Size = size // the size limit is the count of entries
+        });
+
+    /// <summary>
+    ///     Adds a key-value to the cache.
     /// </summary>
     public void Add<T>(string cacheKey, T value, int size = 1)
-    {
-        _memoryCache.Set(cacheKey, value, new MemoryCacheEntryOptions
+        => _memoryCache.Set(cacheKey, value, new MemoryCacheEntryOptions
         {
             Size = size // the size limit is the count of entries
         });
-    }
 
     /// <summary>
-    /// A thread-safe way of working with memory cache. First tries to get the key's value from the cache.
-    /// Otherwise it will use the factory method to get the value and then inserts it.
+    ///     A thread-safe way of working with memory cache. First tries to get the key's value from the cache.
+    ///     Otherwise it will use the factory method to get the value and then inserts it.
     /// </summary>
     public T? GetOrAdd<T>(string cacheKey, Func<T> factory, DateTimeOffset absoluteExpiration, int size = 1)
+        => GetOrAdd(cacheKey, factory, new MemoryCacheEntryOptions
+        {
+            AbsoluteExpiration = absoluteExpiration,
+            Size = size // the size limit is the count of entries
+        });
+
+    /// <summary>
+    ///     A thread-safe way of working with memory cache. First tries to get the key's value from the cache.
+    ///     Otherwise it will use the factory method to get the value and then inserts it.
+    /// </summary>
+    public T? GetOrAdd<T>(string cacheKey, Func<T> factory, TimeSpan absoluteExpirationRelativeToNow, int size = 1)
+        => GetOrAdd(cacheKey, factory, new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow,
+            Size = size // the size limit is the count of entries
+        });
+
+    /// <summary>
+    ///     Removes the object associated with the given key.
+    /// </summary>
+    public void Remove(string cacheKey)
+        => _memoryCache.Remove(cacheKey);
+
+    private T? GetOrAdd<T>(string cacheKey, Func<T> factory, MemoryCacheEntryOptions options)
     {
         if (factory == null)
         {
@@ -101,26 +141,15 @@ public class MemoryCacheService : ICacheService
             }
 
             result = factory();
-            _memoryCache.Set(cacheKey, result, new MemoryCacheEntryOptions
-            {
-                AbsoluteExpiration = absoluteExpiration,
-                Size = size // the size limit is the count of entries
-            });
+
+            _memoryCache.Set(cacheKey, result, options);
 
             return result;
         }
     }
 
-    /// <summary>
-    /// Removes the object associated with the given key.
-    /// </summary>
-    public void Remove(string cacheKey)
-    {
-        _memoryCache.Remove(cacheKey);
-    }
-
     private static class TypeLock<T>
     {
-        public static object Lock { get; } = new object();
+        public static object Lock { get; } = new();
     }
 }
