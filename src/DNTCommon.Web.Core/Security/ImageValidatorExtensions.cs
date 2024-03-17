b588e0f9a3a00398e67804d9d37ec7c6e392f7b3
VahidN <vahid_nasiri@yahoo.com>
2024-03-17 12:35:15 +0330

    Add Image Validator

---
+++ src/DNTCommon.Web.Core/Security/ImageValidatorExtensions.cs
@@ -0,0 +1,137 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+using SkiaSharp;
+
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     Tries to decode the given data to a bitmap to validate its content
+/// </summary>
+public static class ImageValidatorExtensions
+{
+    /// <summary>
+    ///     Tries to decode the given file to a bitmap
+    /// </summary>
+    /// <param name="filePath">The absolute path of the file</param>
+    /// <param name="maxWidth">maximum allowed width</param>
+    /// <param name="maxHeight">maximum allowed height</param>
+    public static bool IsValidImageFile([NotNullWhen(true)] this string? filePath, int maxWidth, int maxHeight)
+    {
+        if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
+        {
+            return false;
+        }
+
+        try
+        {
+            using var bitmap = SKBitmap.Decode(filePath);
+
+            return bitmap != null && bitmap.Info.Width <= maxWidth && bitmap.Info.Height <= maxHeight;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///     Tries to decode the given bytes to a bitmap
+    /// </summary>
+    /// <param name="data">The provided content</param>
+    /// <param name="maxWidth">maximum allowed width</param>
+    /// <param name="maxHeight">maximum allowed height</param>
+    public static bool IsValidImageFile([NotNullWhen(true)] this byte[]? data, int maxWidth, int maxHeight)
+    {
+        if (data == null || data.Length == 0)
+        {
+            return false;
+        }
+
+        try
+        {
+            using var bitmap = SKBitmap.Decode(data);
+
+            return bitmap != null && bitmap.Info.Width <= maxWidth && bitmap.Info.Height <= maxHeight;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///     Tries to decode the given stream to a bitmap
+    /// </summary>
+    /// <param name="stream">The stream of a given data</param>
+    /// <param name="maxWidth">maximum allowed width</param>
+    /// <param name="maxHeight">maximum allowed height</param>
+    public static bool IsValidImageFile([NotNullWhen(true)] this Stream? stream, int maxWidth, int maxHeight)
+    {
+        if (stream is null)
+        {
+            return false;
+        }
+
+        try
+        {
+            using var inputStream = new SKManagedStream(stream);
+            using var codec = SKCodec.Create(inputStream);
+            using var bitmap = SKBitmap.Decode(codec);
+
+            return bitmap != null && bitmap.Info.Width <= maxWidth && bitmap.Info.Height <= maxHeight;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///     Tries to decode the given posted file to a bitmap
+    /// </summary>
+    /// <param name="fromFile">Represents a file sent with the HttpRequest</param>
+    /// <param name="maxWidth">maximum allowed width</param>
+    /// <param name="maxHeight">maximum allowed height</param>
+    public static bool IsValidImageFile([NotNullWhen(true)] this IFormFile? fromFile, int maxWidth, int maxHeight)
+    {
+        if (fromFile is null || fromFile.Length == 0)
+        {
+            return false;
+        }
+
+        try
+        {
+            using var memoryStream = new MemoryStream();
+            fromFile.CopyTo(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            using var inputStream = new SKManagedStream(memoryStream);
+            using var codec = SKCodec.Create(inputStream);
+            using var bitmap = SKBitmap.Decode(codec);
+
+            return bitmap != null && bitmap.Info.Width <= maxWidth && bitmap.Info.Height <= maxHeight;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///     Tries to decode the given posted files to bitmaps
+    /// </summary>
+    /// <param name="fromFiles">Represents the files sent with the HttpRequest</param>
+    /// <param name="maxWidth">maximum allowed width</param>
+    /// <param name="maxHeight">maximum allowed height</param>
+    public static bool AreValidImageFiles([NotNullWhen(true)] this IFormFileCollection? fromFiles,
+        int maxWidth,
+        int maxHeight)
+    {
+        if (fromFiles is null || fromFiles.Count == 0)
+        {
+            return false;
+        }
+
+        return fromFiles.All(fromFile => fromFile.IsValidImageFile(maxWidth, maxHeight));
+    }
+}
