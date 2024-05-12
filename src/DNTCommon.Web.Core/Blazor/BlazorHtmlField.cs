0f5a69cc697e9dec1c48162652013a3e3174f38d
VahidN <vahid_nasiri@yahoo.com>
2024-05-12 15:54:16 +0330

    Add BlazorHtmlField

---
+++ src/DNTCommon.Web.Core/Blazor/BlazorHtmlField.cs
@@ -0,0 +1,64 @@
+#if NET_8
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     This utility class returns the HtmlFieldName of a given ValueExpression with its HtmlFieldPrefix
+/// </summary>
+public class BlazorHtmlField<T> : InputBase<T>
+{
+    /// <summary>
+    ///     This utility class returns the HtmlFieldName of a given ValueExpression with its HtmlFieldPrefix
+    /// </summary>
+    public BlazorHtmlField(Expression<Func<T>> valueExpression)
+    {
+        ValueExpression = valueExpression;
+        _ = base.SetParametersAsync(ParameterView.FromDictionary(new Dictionary<string, object?>()));
+        HtmlFieldName = NameAttributeValue;
+    }
+
+    /// <summary>
+    ///     Gets the value to be used for the input's "name" attribute.
+    /// </summary>
+    public string HtmlFieldName { get; private set; }
+
+    /// <summary>
+    ///     Gets the name of the related FieldIdentifier.
+    /// </summary>
+    public string FieldIdentifierName => !string.IsNullOrWhiteSpace(FieldIdentifier.FieldName)
+        ? FieldIdentifier.FieldName
+        : Guid.NewGuid().ToString("N");
+
+    /// <summary>
+    /// Parses a string to create an instance of T. Derived classes can override this to change how
+    /// CurrentValueAsString interprets incoming values.
+    /// </summary>
+    protected override bool TryParseValueFromString(string? value, out T result, out string validationErrorMessage)
+        => throw new NotImplementedException();
+
+    /// <summary>
+    ///     Signals that the value for the specified field has changed.
+    ///     Signals that some aspect of validation state has changed.
+    /// </summary>
+    public void NotifyFieldChanged(EditContext? editContext)
+    {
+        if (editContext == null)
+        {
+            return;
+        }
+
+        var fieldIdentifier = FieldIdentifier;
+
+        if (string.IsNullOrWhiteSpace(fieldIdentifier.FieldName))
+        {
+            return;
+        }
+
+        editContext.NotifyFieldChanged(in fieldIdentifier);
+        editContext.NotifyValidationStateChanged();
+    }
+}
+#endif
