8989b66718d7944fb43f1b61dea73e2224588057
VahidN <vahid_nasiri@yahoo.com>
2024-06-29 23:07:42 +0330

    Improve DisplayNameExtensions

--- src/DNTCommon.Web.Core/MiscUtils/DisplayNameExtensions.cs
+++ src/DNTCommon.Web.Core/MiscUtils/DisplayNameExtensions.cs
@@ -8,12 +8,18 @@ public static class DisplayNameExtensions
     /// <summary>
     ///     Gets the DisplayName of the provided expression
     /// </summary>
-    public static string GetDisplayName<TEntity>(this Expression<Func<TEntity>>? expression) => expression.GetDisplayName();
+    public static string GetDisplayName<TEntity>(this Expression<Func<TEntity, object?>>? expression)
+    {
+        var (_, memberExp) = GetPropertyInfo(expression);
+        var displayAttribute = memberExp.Member.GetCustomAttribute<DisplayAttribute>();
+
+        return displayAttribute is null ? string.Empty : displayAttribute.Name ?? memberExp.Member.Name;
+    }
 
     /// <summary>
     ///     Gets the DisplayName of the provided expression
     /// </summary>
-    public static string GetDisplayName(this Expression? expression)
+    public static string GetDisplayName<TEntity>(this Expression<Func<TEntity>>? expression)
     {
         var memberExp = expression.GetMemberExpression();
 
@@ -25,12 +31,7 @@ public static class DisplayNameExtensions
 
         var displayAttribute = memberExp.Member.GetCustomAttribute<DisplayAttribute>();
 
-        if (displayAttribute is null)
-        {
-            return string.Empty;
-        }
-
-        return displayAttribute.Name ?? memberExp.Member.Name;
+        return displayAttribute is null ? string.Empty : displayAttribute.Name ?? memberExp.Member.Name;
     }
 
     /// <summary>
@@ -57,5 +58,35 @@ public static class DisplayNameExtensions
         }
 
         return null;
+    }
+
+    /// <summary>
+    ///     Gets the PropertyInfo of the provided expression
+    /// </summary>
+    public static (PropertyInfo Info, MemberExpression MemberExp) GetPropertyInfo<TEntity>(
+        this Expression<Func<TEntity, object?>>? expression)
+        => expression?.Body switch
+        {
+            null => throw new ArgumentNullException(nameof(expression)),
+            UnaryExpression { Operand: MemberExpression memberExp } => ((PropertyInfo)memberExp.Member, memberExp),
+            MemberExpression memberExp => ((PropertyInfo)memberExp.Member, memberExp),
+            _ => throw new ArgumentException($"The expression doesn't indicate a valid property. [ {expression} ]",
+                nameof(expression))
+        };
+
+    /// <summary>
+    ///     Get the actual return type of expr
+    /// </summary>
+    public static Type? GetObjectType<TEntity>(this Expression<Func<TEntity, object?>>? expression)
+    {
+        if (expression is null)
+        {
+            return null;
+        }
+
+        return expression.Body.NodeType is ExpressionType.Convert or ExpressionType.ConvertChecked &&
+               expression.Body is UnaryExpression unary
+            ? unary.Operand.Type
+            : expression.Body.Type;
     }
 }
