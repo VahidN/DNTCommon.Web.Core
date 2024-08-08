e1b7ea7c2033fc575fe2ca29290d9e1dc4a3adec
VahidN <vahid_nasiri@yahoo.com>
2024-08-08 14:29:10 +0330

    Add StrictSameSiteCookieFixMiddleware

---
+++ src/DNTCommon.Web.Core/Security/StrictSameSiteCookieFixMiddleware.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     https://brockallen.com/2019/01/11/same-site-cookies-asp-net-core-and-external-authentication-providers/
+/// </summary>
+/// <param name="next"></param>
+public class StrictSameSiteCookieFixMiddleware(RequestDelegate next)
+{
+#pragma warning disable MA0137
+
+    // MA0137 : Method returning an awaitable type must use the 'Async' suffix -> well, this is how it should be defined!
+    /// <summary>
+    ///     Invokes the StrictSameSiteCookieFixMiddleware
+    /// </summary>
+    public async Task Invoke(HttpContext ctx)
+#pragma warning restore MA0137
+    {
+        ArgumentNullException.ThrowIfNull(ctx);
+
+        var authenticationSchemeProvider = ctx.RequestServices.GetRequiredService<IAuthenticationSchemeProvider>();
+        var authenticationHandlerProvider = ctx.RequestServices.GetRequiredService<IAuthenticationHandlerProvider>();
+        var schemes = await authenticationSchemeProvider.GetRequestHandlerSchemesAsync();
+
+        foreach (var scheme in schemes)
+        {
+            if (await authenticationHandlerProvider.GetHandlerAsync(ctx, scheme.Name) is not
+                    IAuthenticationRequestHandler handler || !await handler.HandleRequestAsync())
+            {
+                continue;
+            }
+
+            string? location = null;
+
+            if (ctx.Response.StatusCode == 302)
+            {
+                location = ctx.Response.Headers[key: "location"];
+            }
+            else if (string.Equals(ctx.Request.Method, b: "GET", StringComparison.OrdinalIgnoreCase) &&
+                     ctx.Request.Query[key: "skip"].Count == 0)
+            {
+                location = $"{ctx.Request.Path}{ctx.Request.QueryString}&skip=1";
+            }
+
+            if (string.IsNullOrEmpty(location))
+            {
+                return;
+            }
+
+            ctx.Response.ContentType = "text/html";
+            ctx.Response.StatusCode = 200;
+
+            var html = $"""
+                        <html>
+                        <head>
+                           <meta http-equiv='refresh' content='0;url={location}' />
+                        </head>
+                        </html>
+                        """;
+
+            await ctx.Response.WriteAsync(html);
+
+            return;
+        }
+
+        await next(ctx);
+    }
+}
