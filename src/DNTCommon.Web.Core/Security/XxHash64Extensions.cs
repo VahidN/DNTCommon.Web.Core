e0ada84212ed0ceae3aa603aae4fe13d2297ad15
VahidN <vahid_nasiri@yahoo.com>
2024-08-18 14:34:29 +0330

    Improve XxHash64Extensions

--- src/DNTCommon.Web.Core/Security/XxHash64Extensions.cs
+++ src/DNTCommon.Web.Core/Security/XxHash64Extensions.cs
@@ -21,6 +21,16 @@ public static class XxHash64Extensions
     /// <summary>
     ///     Computes the XxHash64 hash of the provided data.
     /// </summary>
+    public static string ToXxHash64(this string data, string prefix)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+
+        return $"{prefix}{data.ToXxHash64():X}";
+    }
+
+    /// <summary>
+    ///     Computes the XxHash64 hash of the provided data.
+    /// </summary>
     public static ulong ToXxHash64(this byte[] data)
     {
         ArgumentNullException.ThrowIfNull(data);
@@ -31,10 +41,30 @@ public static class XxHash64Extensions
     /// <summary>
     ///     Computes the XxHash64 hash of the provided data.
     /// </summary>
+    public static string ToXxHash64(this byte[] data, string prefix)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+
+        return $"{prefix}{data.ToXxHash64():X}";
+    }
+
+    /// <summary>
+    ///     Computes the XxHash64 hash of the provided data.
+    /// </summary>
     public static ulong ToXxHash64(this byte[] data, int offset, int len, uint seed)
     {
         ArgumentNullException.ThrowIfNull(data);
 
         return XxHash64.HashToUInt64(data.AsSpan(offset, len), seed);
+    }
+
+    /// <summary>
+    ///     Computes the XxHash64 hash of the provided data.
+    /// </summary>
+    public static string ToXxHash64(this byte[] data, int offset, int len, uint seed, string prefix)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+
+        return $"{prefix}{data.ToXxHash64(offset, len, seed):X}";
     }
 }
