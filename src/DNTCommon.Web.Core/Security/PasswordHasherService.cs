e673a06c34f211a6e8248d568a18e16d72f0a6cd
VahidN <vahid_nasiri@yahoo.com>
2024-08-27 13:56:07 +0330

    Make it easier to work with PasswordHasherService

--- src/DNTCommon.Web.Core/Security/PasswordHasherService.cs
+++ src/DNTCommon.Web.Core/Security/PasswordHasherService.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace DNTCommon.Web.Core;
 
 /// <summary>
@@ -7,33 +5,17 @@ namespace DNTCommon.Web.Core;
 /// </summary>
 public class PasswordHasherService : IPasswordHasherService
 {
-    private const int SaltSize = 64;
-    private const int KeySize = 64;
-    private const int ExpectedHashedPasswordLength = SaltSize + KeySize;
-    private const int Iterations = 210_000;
-    private readonly HashAlgorithmName _algorithm = HashAlgorithmName.SHA512;
     private readonly RandomNumberGenerator _rand = RandomNumberGenerator.Create();
 
     /// <summary>
     ///     Creates a custom hash based on SHA256CryptoServiceProvider.
     /// </summary>
-    public string GetSha256Hash(string input)
-    {
-        var byteValue = Encoding.UTF8.GetBytes(input);
-        var byteHash = SHA256.HashData(byteValue);
-        Array.Reverse(byteHash);
-        return Convert.ToBase64String(byteHash);
-    }
+    public string GetSha256Hash(string input) => input.GetSha256Hash();
 
     /// <summary>
     ///     Creates a custom hash based on SHA1 CryptoServiceProvider.
     /// </summary>
-    public string GetSha1Hash(string input)
-    {
-        var byteValue = Encoding.UTF8.GetBytes(input);
-        var byteHash = SHA1.HashData(byteValue);
-        return BitConverter.ToString(byteHash).Replace("-", "", StringComparison.OrdinalIgnoreCase).ToUpperInvariant();
-    }
+    public string GetSha1Hash(string input) => input.GetSha1Hash();
 
     /// <summary>
     ///     A cryptographic random number generator
@@ -48,6 +30,7 @@ public class PasswordHasherService : IPasswordHasherService
     {
         var salt = new byte[16];
         _rand.GetBytes(salt);
+
         return salt;
     }
 
@@ -56,20 +39,7 @@ public class PasswordHasherService : IPasswordHasherService
     /// </summary>
     /// <param name="password"></param>
     /// <returns></returns>
-    public string GetPbkdf2Hash(string password)
-    {
-        ArgumentNullException.ThrowIfNull(password);
-
-        Span<byte> hashedPasswordBytes = stackalloc byte[ExpectedHashedPasswordLength];
-
-        var saltBytes = hashedPasswordBytes[..SaltSize];
-        var keyBytes = hashedPasswordBytes.Slice(SaltSize, KeySize);
-
-        RandomNumberGenerator.Fill(saltBytes);
-        Rfc2898DeriveBytes.Pbkdf2(password, saltBytes, keyBytes, Iterations, _algorithm);
-
-        return Convert.ToBase64String(hashedPasswordBytes);
-    }
+    public string GetPbkdf2Hash(string password) => password.GetPbkdf2Hash();
 
     /// <summary>
     ///     Verifies a hash created by GetPbkdf2Hash method.
@@ -78,52 +48,5 @@ public class PasswordHasherService : IPasswordHasherService
     /// <param name="providedPassword"></param>
     /// <returns></returns>
     public bool IsValidPbkdf2Hash(string hashedPassword, string providedPassword)
-    {
-        ArgumentNullException.ThrowIfNull(hashedPassword);
-        ArgumentNullException.ThrowIfNull(providedPassword);
-
-        var hashedPasswordByteCount = ComputeDecodedBase64ByteCount(hashedPassword);
-        Span<byte> hashedPasswordBytes = stackalloc byte[hashedPasswordByteCount];
-
-        if (!Convert.TryFromBase64String(hashedPassword, hashedPasswordBytes, out _))
-        {
-            return false;
-        }
-
-        if (hashedPasswordBytes.Length == 0)
-        {
-            return false;
-        }
-
-        if (hashedPasswordBytes.Length != ExpectedHashedPasswordLength)
-        {
-            return false;
-        }
-
-        var saltBytes = hashedPasswordBytes[..SaltSize];
-        var expectedKeyBytes = hashedPasswordBytes.Slice(SaltSize, KeySize);
-
-        Span<byte> actualKeyBytes = stackalloc byte[KeySize];
-        Rfc2898DeriveBytes.Pbkdf2(providedPassword, saltBytes, actualKeyBytes, Iterations, _algorithm);
-
-        return CryptographicOperations.FixedTimeEquals(expectedKeyBytes, actualKeyBytes);
-    }
-
-    private static int ComputeDecodedBase64ByteCount(string base64Str)
-    {
-        var characterCount = base64Str.Length;
-        var paddingCount = 0;
-
-        if (characterCount > 0 && base64Str[characterCount - 1] == '=')
-        {
-            paddingCount++;
-
-            if (characterCount > 1 && base64Str[characterCount - 2] == '=')
-            {
-                paddingCount++;
-            }
-        }
-
-        return characterCount * 3 / 4 - paddingCount;
-    }
+        => hashedPassword.IsValidPbkdf2Hash(providedPassword);
 }
--- src/DNTCommon.Web.Core/Security/PasswordHasherServiceExtensions.cs
+++ src/DNTCommon.Web.Core/Security/PasswordHasherServiceExtensions.cs
@@ -1,18 +1,127 @@
+using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DNTCommon.Web.Core;
 
 /// <summary>
-/// PasswordHasher Service Extensions
+///     PasswordHasher Service Extensions
 /// </summary>
 public static class PasswordHasherServiceExtensions
 {
+    private const int SaltSize = 64;
+    private const int KeySize = 64;
+    private const int ExpectedHashedPasswordLength = SaltSize + KeySize;
+    private const int Iterations = 210_000;
+    private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA512;
+
     /// <summary>
-    /// Adds IPasswordHasherService to IServiceCollection.
+    ///     Adds IPasswordHasherService to IServiceCollection.
     /// </summary>
     public static IServiceCollection AddPasswordHasherService(this IServiceCollection services)
     {
         services.AddSingleton<IPasswordHasherService, PasswordHasherService>();
+
         return services;
+    }
+
+    /// <summary>
+    ///     Creates a custom hash based on SHA256CryptoServiceProvider.
+    /// </summary>
+    public static string GetSha256Hash(this string input)
+    {
+        var byteValue = Encoding.UTF8.GetBytes(input);
+        var byteHash = SHA256.HashData(byteValue);
+        Array.Reverse(byteHash);
+
+        return Convert.ToBase64String(byteHash);
+    }
+
+    /// <summary>
+    ///     Creates a custom hash based on SHA1 CryptoServiceProvider.
+    /// </summary>
+    public static string GetSha1Hash(this string input)
+    {
+        var byteValue = Encoding.UTF8.GetBytes(input);
+        var byteHash = SHA1.HashData(byteValue);
+
+        return BitConverter.ToString(byteHash)
+            .Replace(oldValue: "-", newValue: "", StringComparison.OrdinalIgnoreCase)
+            .ToUpperInvariant();
+    }
+
+    /// <summary>
+    ///     Computes a salted Pbkdf2 hash. Use IsValidPbkdf2Hash method to validate it.
+    /// </summary>
+    /// <param name="password"></param>
+    /// <returns></returns>
+    public static string GetPbkdf2Hash(this string password)
+    {
+        ArgumentNullException.ThrowIfNull(password);
+
+        Span<byte> hashedPasswordBytes = stackalloc byte[ExpectedHashedPasswordLength];
+
+        var saltBytes = hashedPasswordBytes[..SaltSize];
+        var keyBytes = hashedPasswordBytes.Slice(SaltSize, KeySize);
+
+        RandomNumberGenerator.Fill(saltBytes);
+        Rfc2898DeriveBytes.Pbkdf2(password, saltBytes, keyBytes, Iterations, Algorithm);
+
+        return Convert.ToBase64String(hashedPasswordBytes);
+    }
+
+    /// <summary>
+    ///     Verifies a hash created by GetPbkdf2Hash method.
+    /// </summary>
+    /// <param name="hashedPassword"></param>
+    /// <param name="providedPassword"></param>
+    /// <returns></returns>
+    public static bool IsValidPbkdf2Hash(this string hashedPassword, string providedPassword)
+    {
+        ArgumentNullException.ThrowIfNull(hashedPassword);
+        ArgumentNullException.ThrowIfNull(providedPassword);
+
+        var hashedPasswordByteCount = ComputeDecodedBase64ByteCount(hashedPassword);
+        Span<byte> hashedPasswordBytes = stackalloc byte[hashedPasswordByteCount];
+
+        if (!Convert.TryFromBase64String(hashedPassword, hashedPasswordBytes, out _))
+        {
+            return false;
+        }
+
+        if (hashedPasswordBytes.Length == 0)
+        {
+            return false;
+        }
+
+        if (hashedPasswordBytes.Length != ExpectedHashedPasswordLength)
+        {
+            return false;
+        }
+
+        var saltBytes = hashedPasswordBytes[..SaltSize];
+        var expectedKeyBytes = hashedPasswordBytes.Slice(SaltSize, KeySize);
+
+        Span<byte> actualKeyBytes = stackalloc byte[KeySize];
+        Rfc2898DeriveBytes.Pbkdf2(providedPassword, saltBytes, actualKeyBytes, Iterations, Algorithm);
+
+        return CryptographicOperations.FixedTimeEquals(expectedKeyBytes, actualKeyBytes);
+    }
+
+    private static int ComputeDecodedBase64ByteCount(string base64Str)
+    {
+        var characterCount = base64Str.Length;
+        var paddingCount = 0;
+
+        if (characterCount > 0 && base64Str[characterCount - 1] == '=')
+        {
+            paddingCount++;
+
+            if (characterCount > 1 && base64Str[characterCount - 2] == '=')
+            {
+                paddingCount++;
+            }
+        }
+
+        return characterCount * 3 / 4 - paddingCount;
     }
 }
