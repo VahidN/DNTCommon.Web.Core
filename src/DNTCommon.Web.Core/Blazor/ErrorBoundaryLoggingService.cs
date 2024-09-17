f3d9fba457c41d679d0d241d5bfdce221d760118
VahidN <vahid_nasiri@yahoo.com>
2024-09-17 12:35:43 +0330

    Log stacktrace when http context is null

--- src/DNTCommon.Web.Core/Blazor/ErrorBoundaryLoggingService.cs
+++ src/DNTCommon.Web.Core/Blazor/ErrorBoundaryLoggingService.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace DNTCommon.Web.Core;
@@ -6,12 +7,20 @@ namespace DNTCommon.Web.Core;
 /// <summary>
 ///     Logs exception information for a ErrorBoundary component.
 /// </summary>
-public class ErrorBoundaryLoggingService(ILogger<ErrorBoundaryLoggingService> logger) : IErrorBoundaryLogger
+public class ErrorBoundaryLoggingService(
+    ILogger<ErrorBoundaryLoggingService> logger,
+    IHttpContextAccessor httpContextAccessor,
+    IEnhancedStackTraceService enhancedStackTraceService) : IErrorBoundaryLogger
 {
     /// <inheritdoc />
     public ValueTask LogErrorAsync(Exception exception)
     {
-        logger.LogError(exception.Demystify(), message: "Unhandled exception rendering component");
+        var stackTrace = httpContextAccessor.HttpContext is null
+            ? enhancedStackTraceService.GetCurrentStackTrace(declaringType => false)
+            : "";
+
+        logger.LogError(exception.Demystify(), message: "Unhandled exception rendering component {StackTrace}",
+            stackTrace);
 
         return ValueTask.CompletedTask;
     }
