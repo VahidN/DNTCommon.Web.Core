d03f6edb011d4b4bed0993939b8e6f39a17a9628
VahidN <vahid_nasiri@yahoo.com>
2024-09-22 11:50:34 +0330

    Add Gen204Controller

---
+++ src/DNTCommon.Web.Core/Middlewares/Gen204Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     Just to stop `/gen204?invalidResponse` errors!
+/// </summary>
+[ApiController]
+[AllowAnonymous]
+[Route(template: "[controller]")]
+public class Gen204Controller : ControllerBase
+{
+    /// <summary>
+    ///     Just to stop `/gen204?invalidResponse` errors!
+    /// </summary>
+    [HttpGet]
+    [HttpPost]
+    public IActionResult Get() => Content(content: "ok");
+}
