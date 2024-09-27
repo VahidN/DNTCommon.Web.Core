6331e5b678ef4dcdaed996578912682db9dd41cc
VahidN <vahid_nasiri@yahoo.com>
2024-09-27 15:15:07 +0330

    Add more help about GeneratePngFromHtmlAsync

--- src/DNTCommon.Web.Core/Chromium/IHtmlToPngGenerator.cs
+++ src/DNTCommon.Web.Core/Chromium/IHtmlToPngGenerator.cs
@@ -7,6 +7,17 @@ public interface IHtmlToPngGenerator
 {
     /// <summary>
     ///     High level method that converts HTML to PNG.
+    ///     This method uses Google-Chrome to take screenshots of the given URLs.
+    ///     You can install this browser on Windows systems very easily.
+    ///     On linux systems use these commands:
+    ///     https://gist.github.com/Austcool-Walker/3f898ff04e5e0a9644ab487984e5eeec
+    ///     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    ///     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    ///     sudo apt-get update
+    ///     sudo apt-get install google-chrome-stable
+    ///     
+    ///     google-chrome --version
+    ///     
     /// </summary>
     Task<string> GeneratePngFromHtmlAsync(HtmlToPngGeneratorOptions options);
 }
