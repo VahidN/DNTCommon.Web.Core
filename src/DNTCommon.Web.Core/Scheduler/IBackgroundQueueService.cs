075acdab83f9c977b7fafcfa168a8dfcdd931387
VahidN <vahid_nasiri@yahoo.com>
2024-10-13 19:30:56 +0330

    Improve comments of IBackgroundQueueService

--- src/DNTCommon.Web.Core/Scheduler/IBackgroundQueueService.cs
+++ src/DNTCommon.Web.Core/Scheduler/IBackgroundQueueService.cs
@@ -8,12 +8,14 @@ public interface IBackgroundQueueService : IDisposable
     /// <summary>
     ///     Schedules a task which can run in the background, independent of any request.
     ///     The provided IServiceProvider is created from an IServiceScope.
+    ///     Usage: _queueService.QueueBackgroundWorkItem(async (cancellationToken, serviceProvider) => {    });
     /// </summary>
     void QueueBackgroundWorkItem(Func<CancellationToken, IServiceProvider, Task> workItem);
 
     /// <summary>
     ///     Schedules a task which can run in the background, independent of any request.
     ///     The provided IServiceProvider is created from an IServiceScope.
+    ///     Usage: _queueService.QueueBackgroundWorkItem((cancellationToken, serviceProvider) => {    });
     /// </summary>
     void QueueBackgroundWorkItem(Action<CancellationToken, IServiceProvider> workItem);
 }
