10e4e1a134f59456cc41fce26a0369bfdd342d8f
VahidN <vahid_nasiri@yahoo.com>
2024-10-14 12:48:59 +0330

    Improve ProtectionProviderService

--- src/DNTCommon.Web.Core/Security/ProtectionProviderService.cs
+++ src/DNTCommon.Web.Core/Security/ProtectionProviderService.cs
@@ -1,96 +1,80 @@
-using System;
-using System.Diagnostics;
-using System.Security.Cryptography;
-using System.Text;
 using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
 namespace DNTCommon.Web.Core;
 
 /// <summary>
-/// Protection Provider Service
+///     Protection Provider Service
 /// </summary>
 public class ProtectionProviderService : IProtectionProviderService
 {
-    private readonly ILogger<ProtectionProviderService> _logger;
     private readonly IDataProtector _dataProtector;
+    private readonly ILogger<ProtectionProviderService> _logger;
     private readonly ISerializationProvider _serializationProvider;
 
     /// <summary>
-    /// The default protection provider
+    ///     The default protection provider
     /// </summary>
-    public ProtectionProviderService(
-        IDataProtectionProvider dataProtectionProvider,
+    public ProtectionProviderService(IDataProtectionProvider dataProtectionProvider,
         ILogger<ProtectionProviderService> logger,
         ISerializationProvider serializationProvider)
     {
-        if (dataProtectionProvider == null)
-        {
-            throw new ArgumentNullException(nameof(dataProtectionProvider));
-        }
+        ArgumentNullException.ThrowIfNull(dataProtectionProvider);
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        _dataProtector = dataProtectionProvider.CreateProtector(typeof(ProtectionProviderService).FullName ?? nameof(ProtectionProviderService));
-        _serializationProvider = serializationProvider ?? throw new ArgumentNullException(nameof(serializationProvider));
+
+        _dataProtector =
+            dataProtectionProvider.CreateProtector(typeof(ProtectionProviderService).FullName ??
+                                                   nameof(ProtectionProviderService));
+
+        _serializationProvider =
+            serializationProvider ?? throw new ArgumentNullException(nameof(serializationProvider));
     }
 
     /// <summary>
-    /// Decrypts the message
+    ///     Decrypts the message
     /// </summary>
     public string? Decrypt(string inputText)
     {
-        if (inputText == null)
-        {
-            throw new ArgumentNullException(nameof(inputText));
-        }
+        ArgumentNullException.ThrowIfNull(inputText);
 
         try
         {
-            var inputBytes = WebEncoders.Base64UrlDecode(inputText);
-            var bytes = _dataProtector.Unprotect(inputBytes);
-            return Encoding.UTF8.GetString(bytes);
+            return _dataProtector.Unprotect(inputText);
         }
         catch (FormatException ex)
         {
-            _logger.LogError(ex.Demystify(), "Invalid base 64 string. Fall through.");
+            _logger.LogError(ex.Demystify(), message: "Invalid base 64 string. Fall through.");
         }
         catch (CryptographicException ex)
         {
-            _logger.LogError(ex.Demystify(), "Invalid protected payload. Fall through.");
+            _logger.LogError(ex.Demystify(), message: "Invalid protected payload. Fall through.");
         }
 
         return null;
     }
 
     /// <summary>
-    /// Encrypts the message
+    ///     Encrypts the message
     /// </summary>
     public string Encrypt(string inputText)
     {
-        if (inputText == null)
-        {
-            throw new ArgumentNullException(nameof(inputText));
-        }
+        ArgumentNullException.ThrowIfNull(inputText);
 
-        var inputBytes = Encoding.UTF8.GetBytes(inputText);
-        var bytes = _dataProtector.Protect(inputBytes);
-        return WebEncoders.Base64UrlEncode(bytes);
+        return _dataProtector.Protect(inputText);
     }
 
     /// <summary>
-    /// It will serialize an object as a JSON string and then encrypt it as a Base64UrlEncode string.
+    ///     It will serialize an object as a JSON string and then encrypt it as a Base64UrlEncode string.
     /// </summary>
-    public string EncryptObject(object data)
-    {
-        return Encrypt(_serializationProvider.Serialize(data));
-    }
+    public string EncryptObject(object data) => Encrypt(_serializationProvider.Serialize(data));
 
     /// <summary>
-    /// It will decrypt a Base64UrlEncode encrypted JSON string and then deserialize it as an object.
+    ///     It will decrypt a Base64UrlEncode encrypted JSON string and then deserialize it as an object.
     /// </summary>
     public T? DecryptObject<T>(string data)
     {
         var decryptData = Decrypt(data);
+
         return decryptData == null ? default : _serializationProvider.Deserialize<T>(decryptData);
     }
 }
