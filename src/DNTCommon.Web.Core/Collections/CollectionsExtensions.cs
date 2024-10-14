998d9c5a013b433e1ec0873117f540da5f032e64
VahidN <vahid_nasiri@yahoo.com>
2024-10-14 12:59:52 +0330

    Improve CollectionsExtensions

--- src/DNTCommon.Web.Core/Collections/CollectionsExtensions.cs
+++ src/DNTCommon.Web.Core/Collections/CollectionsExtensions.cs
@@ -1,37 +1,65 @@
-using System;
-using System.Collections.Generic;
-
 namespace DNTCommon.Web.Core;
 
 /// <summary>
-/// Collections Extensions
+///     Collections Extensions
 /// </summary>
 public static class CollectionsExtensions
 {
     /// <summary>
-    /// Adds the `AddRange` to an `IList`
+    ///     Adds the `AddRange` to an `IList`
     /// </summary>
-    public static void AddRange<T>(this IList<T> source, IEnumerable<T> newList)
+    public static bool AddRange<T>([NotNullWhen(returnValue: true)] this ICollection<T>? source,
+        [NotNullWhen(returnValue: true)] IEnumerable<T>? newList)
     {
-        if (source == null)
+        if (source is null || newList is null)
         {
-            throw new ArgumentNullException(nameof(source));
-        }
-
-        if (newList == null)
-        {
-            throw new ArgumentNullException(nameof(newList));
+            return false;
         }
 
         if (source is List<T> concreteList)
         {
             concreteList.AddRange(newList);
-            return;
+
+            return true;
         }
 
         foreach (var element in newList)
         {
             source.Add(element);
         }
+
+        return true;
+    }
+
+    /// <summary>
+    ///     Return true if the number of elements contained in the source is > 0
+    /// </summary>
+    public static bool IsNullOrEmpty<T>([NotNullWhen(returnValue: false)] this ICollection<T>? source)
+        => source == null || source.Count == 0;
+
+    /// <summary>
+    ///     Return true if the number of elements contained in the source is > 0
+    /// </summary>
+    public static bool IsNullOrEmpty<T>([NotNullWhen(returnValue: false)] this IEnumerable<T>? source)
+        => source == null || !source.Any();
+
+    /// <summary>
+    ///     Return true if the source elements are equal to destination elements
+    /// </summary>
+    public static bool IsEqualTo<T>(this IList<T>? source, IList<T>? destination)
+    {
+        if (source == null || destination == null)
+        {
+            return source == null && destination == null;
+        }
+
+        if (source.Count != destination.Count)
+        {
+            return false;
+        }
+
+        var comparer = EqualityComparer<T>.Default;
+
+        return !source.Where((t, i) => !comparer.Equals(t, destination[i])).Any();
     }
 }
