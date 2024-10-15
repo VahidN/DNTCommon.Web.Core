9046e657eed2080a2df2a702c52e112f66e4a9fe
VahidN <vahid_nasiri@yahoo.com>
2024-10-15 20:19:29 +0330

    Add TypeExtensions

---
+++ src/DNTCommon.Web.Core/MiscUtils/TypeExtensions.cs
@@ -0,0 +1,174 @@
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     Provides extension methods for Type.
+/// </summary>
+public static class TypeExtensions
+{
+	/// <summary>
+	///     Determines if the type implements the given interface.
+	/// </summary>
+	public static bool Implements([NotNullWhen(returnValue: true)] this Type? type,
+        [NotNullWhen(returnValue: true)] Type? interfaceType)
+        => type is not null && interfaceType?.IsInterface == true && type.GetInterfaces().Any(i => i == interfaceType);
+
+	/// <summary>
+	///     Determines if the type implements the given interface.
+	/// </summary>
+	public static bool Implements<TInterface>([NotNullWhen(returnValue: true)] this Type? type)
+        => type.Implements(typeof(TInterface));
+
+	/// <summary>
+	///     Determines if the type is derived from the given base type.
+	/// </summary>
+	public static bool IsDerivedFrom([NotNullWhen(returnValue: true)] this Type? type,
+        [NotNullWhen(returnValue: true)] Type? baseType)
+        => baseType?.IsAssignableFrom(type) == true;
+
+	/// <summary>
+	///     Determines if the type is derived from the given base type.
+	/// </summary>
+	public static bool IsDerivedFrom<TBaseType>([NotNullWhen(returnValue: true)] this Type? type)
+        => type.IsDerivedFrom(typeof(TBaseType));
+
+	/// <summary>
+	///     Finds all the derived concrete types of baseType in the given assemblies
+	/// </summary>
+	public static IList<Type> GetAllDerivedConcreteTypes(this ICollection<Assembly>? assemblies, Type baseType)
+    {
+        if (assemblies is null)
+        {
+            return [];
+        }
+
+        return assemblies.Where(a => !a.IsDynamic)
+            .SelectMany(a => a.GetExportedTypes())
+            .Where(t => t is { IsAbstract: false, IsInterface: false } && baseType.IsAssignableFrom(t))
+            .ToList();
+    }
+
+	/// <summary>
+	///     Finds all the derived types of baseType in the given assemblies
+	/// </summary>
+	public static IList<Type> GetAllDerivedConcreteTypes<TBaseType>(this ICollection<Assembly>? assemblies)
+        => assemblies.GetAllDerivedConcreteTypes(typeof(TBaseType));
+
+	/// <summary>
+	///     Finds all the derived types of baseType in the given assemblies
+	/// </summary>
+	public static IList<Type> GetAllDerivedConcreteTypes<TBaseType>(this Assembly assembly)
+        => new List<Assembly>
+        {
+            assembly
+        }.GetAllDerivedConcreteTypes(typeof(TBaseType));
+
+	/// <summary>
+	///     Finds all the derived types of baseType in the given assemblies
+	/// </summary>
+	public static IList<Type> GetAllDerivedConcreteTypes(this Assembly assembly, Type baseType)
+        => new List<Assembly>
+        {
+            assembly
+        }.GetAllDerivedConcreteTypes(baseType);
+
+	/// <summary>
+	///     Determines if the type is derived from the given base type or implements the given interface.
+	/// </summary>
+	public static bool IsDerivedFromOrImplements([NotNullWhen(returnValue: true)] this Type? type,
+        [NotNullWhen(returnValue: true)] Type? baseType)
+        => baseType?.IsInterface == true ? type.Implements(baseType) : type.IsDerivedFrom(baseType);
+
+	/// <summary>
+	///     Determines if the type is derived from the given base type or implements the given interface.
+	/// </summary>
+	public static bool IsDerivedFromOrImplements<TBaseType>([NotNullWhen(returnValue: true)] this Type? type)
+        => type.IsDerivedFromOrImplements(typeof(TBaseType));
+
+	/// <summary>
+	///     Determines if the type is an instance of a generic type.
+	/// </summary>
+	public static bool IsDerivedFromGenericType([NotNullWhen(returnValue: true)] this Type? type,
+        [NotNullWhen(returnValue: true)] Type? genericType)
+    {
+        var typeTmp = type;
+
+        while (typeTmp != null)
+        {
+            if (typeTmp.IsGenericType && typeTmp.GetGenericTypeDefinition() == genericType)
+            {
+                return true;
+            }
+
+            typeTmp = typeTmp.BaseType;
+        }
+
+        return false;
+    }
+
+	/// <summary>
+	///     Gets the underlying type if the type is Nullable, otherwise just returns the type.
+	/// </summary>
+	public static Type? GetTrueType([NotNullIfNotNull(nameof(type))] this Type? type)
+    {
+        if (type is null)
+        {
+            return null;
+        }
+
+        return IsDerivedFromGenericType(type, typeof(Nullable<>)) ? type.GetGenericArguments()[0] : type;
+    }
+
+	/// <summary>
+	///     Gets the fields that are of the specified type.
+	/// </summary>
+	public static IList<FieldInfo> GetFieldsDerivedFrom<T>(this Type? type, BindingFlags flags)
+    {
+        var fieldsOf = new List<FieldInfo>();
+
+        if (type is null)
+        {
+            return fieldsOf;
+        }
+
+        fieldsOf.AddRange(type.GetFields(flags).Where(fld => fld.FieldType.IsDerivedFrom(typeof(T))));
+
+        return fieldsOf;
+    }
+
+	/// <summary>
+	///     Gets the properties that are of the specified type.
+	/// </summary>
+	public static IList<PropertyInfo> GetPropertiesDerivedFrom<T>(this Type? type, BindingFlags flags)
+    {
+        var fieldsOf = new List<PropertyInfo>();
+
+        if (type is null)
+        {
+            return fieldsOf;
+        }
+
+        fieldsOf.AddRange(type.GetProperties(flags).Where(fld => fld.PropertyType.IsDerivedFrom(typeof(T))));
+
+        return fieldsOf;
+    }
+
+	/// <summary>
+	///     Determines if the type supports null.
+	/// </summary>
+	/// <param name="type"></param>
+	/// <returns></returns>
+	public static bool IsNullable([NotNullWhen(returnValue: true)] this Type? type)
+        => type != null && (!type.IsValueType || Nullable.GetUnderlyingType(type) != null);
+
+	/// <summary>
+	///     Determines if the type has the specified attribute.
+	/// </summary>
+	public static bool HasAttribute<TAttribute>([NotNullWhen(returnValue: true)] this Type? type, bool inherited = true)
+        => type?.GetTypeInfo().GetCustomAttributes(inherited).OfType<TAttribute>().Any() == true;
+
+	/// <summary>
+	///     Determines if the type !IsAbstract and !IsInterface.
+	/// </summary>
+	public static bool IsConcreteType([NotNullWhen(returnValue: true)] this Type? type)
+        => type?.GetTypeInfo() is { IsAbstract: false, IsInterface: false };
+}
