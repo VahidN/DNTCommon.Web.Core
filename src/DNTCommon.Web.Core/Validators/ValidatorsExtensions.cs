38b4f6c172154a62988b0a75f6b677a353c0ad86
VahidN <vahid_nasiri@yahoo.com>
2025-05-25 13:35:44 +0330

    Improve IsValidGuid

--- src/DNTCommon.Web.Core/Validators/ValidatorsExtensions.cs
+++ src/DNTCommon.Web.Core/Validators/ValidatorsExtensions.cs
@@ -8,6 +8,6 @@ public static class ValidatorsExtensions
     /// <summary>
     ///     Tries to parse a string into a Guid-value.
     /// </summary>
-    public static bool IsValidGuid(this string value)
+    public static bool IsValidGuid(this string? value)
         => !value.IsEmpty() && Guid.TryParse(value, CultureInfo.InvariantCulture, out _);
 }
