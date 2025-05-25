9a130d237f46343dfda7c4ef40f5212e655c8424
VahidN <vahid_nasiri@yahoo.com>
2025-05-25 20:15:41 +0330

    Adjust ValidatorsExtensions for the newer .NET versions

--- src/DNTCommon.Web.Core/Validators/ValidatorsExtensions.cs
+++ src/DNTCommon.Web.Core/Validators/ValidatorsExtensions.cs
@@ -5,9 +5,11 @@ namespace DNTCommon.Web.Core;
 /// </summary>
 public static class ValidatorsExtensions
 {
+#if !NET_6
     /// <summary>
     ///     Tries to parse a string into a Guid-value.
     /// </summary>
     public static bool IsValidGuid(this string? value)
         => !value.IsEmpty() && Guid.TryParse(value, CultureInfo.InvariantCulture, out _);
+#endif
 }
