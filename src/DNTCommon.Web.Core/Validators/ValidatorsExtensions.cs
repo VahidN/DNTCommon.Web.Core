dd1e06c2370aec14477176b9b64e3262e4c22d0d
VahidN <vahid_nasiri@yahoo.com>
2025-05-25 13:14:33 +0330

    Add ValidatorsExtensions

---
+++ src/DNTCommon.Web.Core/Validators/ValidatorsExtensions.cs
@@ -0,0 +1,13 @@
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     Validators Extensions
+/// </summary>
+public static class ValidatorsExtensions
+{
+    /// <summary>
+    ///     Tries to parse a string into a Guid-value.
+    /// </summary>
+    public static bool IsValidGuid(this string value)
+        => !value.IsEmpty() && Guid.TryParse(value, CultureInfo.InvariantCulture, out _);
+}
