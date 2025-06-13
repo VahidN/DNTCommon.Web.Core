0133443952c39e2633ec4e6c3f4f36d931f6d672
VahidN <vahid_nasiri@yahoo.com>
2025-06-13 19:39:38 +0330

    Improve CleanupActionStringValues, Close #144

--- src/DNTCommon.Web.Core/ActionFilters/FilterAttributesExtensions.cs
+++ src/DNTCommon.Web.Core/ActionFilters/FilterAttributesExtensions.cs
@@ -10,44 +10,80 @@ public static class FilterAttributesExtensions
     private static readonly Type StringType = typeof(string);
 
     /// <summary>
-    ///     Cleans all string values in the current ActionArguments and model's string properties using the specified function.
+    ///     Cleans all string values in the current ActionArguments, including collections of strings and model's string
+    ///     properties, using the specified function.
     /// </summary>
     public static void CleanupActionStringValues(this ActionExecutingContext context, Func<string, string> action)
     {
         ArgumentNullException.ThrowIfNull(context);
         ArgumentNullException.ThrowIfNull(action);
 
-        foreach (var (aKey, aValue) in context.ActionArguments)
+        foreach (var (key, argumentValue) in context.ActionArguments)
         {
-            if (aValue is null)
+            switch (argumentValue)
             {
-                continue;
-            }
+                case null:
+                    continue;
 
-            var type = aValue.GetType();
+                // Case 1: The argument is a simple string.
+                case string strValue:
+                    context.ActionArguments[key] = action(strValue);
 
-            if (type == StringType)
-            {
-                context.ActionArguments[aKey] = action(Convert.ToString(aValue, CultureInfo.InvariantCulture) ?? "");
-            }
-            else
-            {
-                var stringProperties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                    .Where(x => x.CanRead && x.CanWrite && x.PropertyType == StringType &&
-                                x.GetGetMethod(nonPublic: true)?.IsPublic == true &&
-                                x.GetSetMethod(nonPublic: true)?.IsPublic == true);
+                    break;
 
-                foreach (var propertyInfo in stringProperties)
-                {
-                    var value = propertyInfo.GetValue(aValue);
-
-                    if (value is null)
+                // Case 2: The argument is a list of strings.
+                case List<string> stringList:
+                    for (var i = 0; i < stringList.Count; i++)
                     {
-                        continue;
+                        stringList[i] = action(stringList[i]);
                     }
 
-                    propertyInfo.SetValue(aValue, action(Convert.ToString(value, CultureInfo.InvariantCulture) ?? ""));
-                }
+                    break;
+
+                // Case 3: The argument is an array of strings.
+                case string[] stringArray:
+                    for (var i = 0; i < stringArray.Length; i++)
+                    {
+                        stringArray[i] = action(stringArray[i]);
+                    }
+
+                    break;
+
+                // Case 4: The argument is a jagged array of strings (array of string arrays).
+                case string[][] jaggedStringArray:
+                    foreach (var innerArray in jaggedStringArray)
+                    {
+                        if (innerArray is null)
+                        {
+                            continue;
+                        }
+
+                        for (var j = 0; j < innerArray.Length; j++)
+                        {
+                            innerArray[j] = action(innerArray[j]);
+                        }
+                    }
+
+                    break;
+
+                // Case 5: The argument is a complex object.
+                // This part remains the same to handle string properties of a model.
+                default:
+                    foreach (var stringProperty in argumentValue.GetType()
+                                 .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                                 .Where(p => p is { CanRead: true, CanWrite: true } && p.PropertyType == StringType &&
+                                             p.GetGetMethod(nonPublic: true)?.IsPublic == true &&
+                                             p.GetSetMethod(nonPublic: true)?.IsPublic == true))
+                    {
+                        var value = stringProperty.GetValue(argumentValue);
+
+                        if (value is string propValue)
+                        {
+                            stringProperty.SetValue(argumentValue, action(propValue));
+                        }
+                    }
+
+                    break;
             }
         }
     }
