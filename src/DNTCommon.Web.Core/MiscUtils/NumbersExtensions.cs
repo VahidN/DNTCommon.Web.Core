47aa42e137efff1f7421401f146f660a510b9853
VahidN <vahid_nasiri@yahoo.com>
2025-06-27 14:15:23 +0330

    Improve NumbersExtensions

--- src/DNTCommon.Web.Core/MiscUtils/NumbersExtensions.cs
+++ src/DNTCommon.Web.Core/MiscUtils/NumbersExtensions.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace DNTCommon.Web.Core;
 
 /// <summary>
@@ -18,6 +20,55 @@ public static class NumbersExtensions
             return defaultValue;
         }
 
-        return int.TryParse(data, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var result) ? result : defaultValue;
+        return int.TryParse(data, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var result)
+            ? result
+            : defaultValue;
     }
+
+#if !NET_6
+
+    /// <summary>
+    ///     Determines if a value represents an integral number.
+    /// </summary>
+    public static bool IsInteger<T>(this T value)
+        where T : INumber<T>
+        => T.IsInteger(value);
+
+    /// <summary>
+    ///     Determines if a value represents a real number.
+    /// </summary>
+    public static bool IsRealNumber<T>(this T value)
+        where T : INumber<T>
+        => T.IsRealNumber(value);
+
+    /// <summary>
+    ///     Tries to convert a string value to a number
+    /// </summary>
+    public static T? ToNumber<T>(this string? data, NumberStyles style = NumberStyles.AllowDecimalPoint)
+        where T : INumber<T>
+    {
+        var defaultValue = default(T);
+
+        if (string.IsNullOrWhiteSpace(data))
+        {
+            return defaultValue;
+        }
+
+        return T.TryParse(data, style, CultureInfo.InvariantCulture, out var result) ? result : defaultValue;
+    }
+
+    /// <summary>
+    ///     Converts Pounds to Kg
+    /// </summary>
+    public static T PoundsToKg<T>(T pounds)
+        where T : INumber<T>
+        => pounds * T.CreateChecked(value: 0.453592f);
+
+    /// <summary>
+    ///     Converts Inches to Cm
+    /// </summary>
+    public static T InchesToCm<T>(T inches)
+        where T : INumber<T>
+        => inches * T.CreateChecked(value: 2.54f);
+#endif
 }
