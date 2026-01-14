ac43f9df4cd507e83b89d077b77ef0e1375f45c9
VahidN <vahid_nasiri@yahoo.com>
2026-01-14 12:57:40 +0330

    Add IsNewSdkVersionAvailable()

--- src/DNTCommon.Web.Core/WebServer/DotNetInfoProvider.cs
+++ src/DNTCommon.Web.Core/WebServer/DotNetInfoProvider.cs
@@ -48,4 +48,17 @@ public static class DotNetInfoProvider
             WaitForExit = TimeSpan.FromSeconds(value: 3),
             KillProcessOnStart = false
         }.ExecuteProcess();
+
+    /// <summary>
+    ///     Returns output of `dotnet sdk check`
+    ///     https://github.com/dotnet/sdk/blob/a34f1ca17979f6cb283ad74c53ca68f8575fceb9/src/Cli/dotnet/commands/dotnet-sdk/check/LocalizableStrings.resx
+    /// </summary>
+    public static bool IsNewSdkVersionAvailable()
+    {
+        var info = GetSdkCheckInfo();
+
+        return info.Contains(value: "is available", StringComparison.OrdinalIgnoreCase) ||
+               info.Contains(value: "patch", StringComparison.OrdinalIgnoreCase) || (!OperatingSystem.IsLinux() &&
+                   info.Contains(value: "newest", StringComparison.OrdinalIgnoreCase));
+    }
 }
