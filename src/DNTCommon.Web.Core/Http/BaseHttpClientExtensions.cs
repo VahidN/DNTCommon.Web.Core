ae2b2d6e49a35114616670475b8dd9c028fddc9c
VahidN <vahid_nasiri@yahoo.com>
2026-01-21 14:13:31 +0330

    Improve BaseHttpClientExtensions

--- src/DNTCommon.Web.Core/Http/BaseHttpClientExtensions.cs
+++ src/DNTCommon.Web.Core/Http/BaseHttpClientExtensions.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Http;
 using Polly;
+using Polly.Timeout;
 
 namespace DNTCommon.Web.Core;
 
@@ -11,73 +12,62 @@ namespace DNTCommon.Web.Core;
 public static class BaseHttpClientExtensions
 {
     /// <summary>
-    ///     Gets the cookie container used to store server cookies by the handler.
-    /// </summary>
-    /// <returns></returns>
-    public static readonly CookieContainer CookieContainer = new();
-
-    /// <summary>
     ///     Adds BaseHttpClient to IServiceCollection
     /// </summary>
     public static IServiceCollection AddBaseHttpClient(this IServiceCollection services)
     {
         services.AddHttpClient<BaseHttpClient>(AddDefaultSettings)
-            .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync([
-                // transient errors: network failures and HTTP 5xx and HTTP 408 errors
-                TimeSpan.FromSeconds(value: 3)
-            ]))
-            .ConfigurePrimaryHttpMessageHandler(() => GetDefaultSocketsHttpHandler(allowAutoRedirect: true));
+            .ConfigurePrimaryHttpMessageHandler(() => CreateScrapingHandler(allowAutoRedirect: true))
+            .AddPolicyHandler(GetTimeoutPolicy())
+            .AddPolicyHandler(GetRetryPolicy())
+            .AddPolicyHandler(GetRateLimitPolicy());
 
         services.AddHttpClient<BaseHttpClientWithoutAutoRedirect>(AddDefaultSettings)
-            .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync([
-                // transient errors: network failures and HTTP 5xx HTTP 408 errors
-                TimeSpan.FromSeconds(value: 3)
-            ]))
-            .ConfigurePrimaryHttpMessageHandler(() => GetDefaultSocketsHttpHandler(allowAutoRedirect: false));
+            .ConfigurePrimaryHttpMessageHandler(() => CreateScrapingHandler(allowAutoRedirect: false))
+            .AddPolicyHandler(GetTimeoutPolicy())
+            .AddPolicyHandler(GetRetryPolicy())
+            .AddPolicyHandler(GetRateLimitPolicy());
 
         services.RemoveAll<IHttpMessageHandlerBuilderFilter>(); // Remove logging of the HttpClient
 
         return services;
     }
 
-    /// <summary>
-    ///     We should not supply the factory with a single instance of HttpClientHandler to use in all clients.
-    ///     Once the default HandlerLifetime has elapsed (1 minutes) it will be marked for disposal,
-    ///     with the actual disposal occurring after all existing HttpClients referencing it are disposed.
-    ///     All clients created after the handler is marked continue to be supplied the soon-to-be disposed handler,
-    ///     leaving them in an invalid state once the disposal is actioned.
-    ///     To fix this, the factory should be configured to create a new handler for each client.
-    /// </summary>
-    /// <returns></returns>
-    private static SocketsHttpHandler GetDefaultSocketsHttpHandler(bool allowAutoRedirect)
-        => new()
-        {
-            AllowAutoRedirect = allowAutoRedirect,
-            CookieContainer = CookieContainer,
-            UseCookies = true,
-            AutomaticDecompression = DecompressionMethods.All,
-            PooledConnectionLifetime = TimeSpan.FromSeconds(value: 10),
-            PooledConnectionIdleTimeout = TimeSpan.FromSeconds(value: 10),
-            MaxConnectionsPerServer = 200,
-            ConnectTimeout = TimeSpan.FromSeconds(value: 10),
-            KeepAlivePingDelay = TimeSpan.FromSeconds(value: 10)
-        };
-
     private static void AddDefaultSettings(HttpClient client)
     {
-        client.Timeout = TimeSpan.FromSeconds(value: 10);
-
-        client.DefaultRequestHeaders.Add(name: "User-Agent",
-            value:
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36");
-
-        client.DefaultRequestHeaders.Add(name: "Keep-Alive", value: "true");
-
-        client.DefaultRequestHeaders.Add(name: "Accept",
-            value: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-
-        client.DefaultRequestHeaders.Add(name: "Accept-Language", value: "en-US,en;q=0.5");
-        client.DefaultRequestHeaders.Add(name: "Accept-Encoding", value: "gzip, deflate, br");
-        client.DefaultRequestHeaders.Add(name: "TE", value: "Trailers");
+        client.DefaultRequestHeaders.UserAgent.ParseAdd(input: "DNTCommon/1.0");
+        client.DefaultRequestHeaders.Accept.ParseAdd(input: "text/html,application/xhtml+xml");
+        client.DefaultRequestHeaders.AcceptLanguage.ParseAdd(input: "en-US,en;q=0.9");
+        client.DefaultRequestHeaders.AcceptEncoding.ParseAdd(input: "gzip, deflate");
     }
+
+    private static AsyncPolicy<HttpResponseMessage> GetTimeoutPolicy()
+        => Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(seconds: 30), TimeoutStrategy.Optimistic);
+
+    private static AsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        => Policy<HttpResponseMessage>.Handle<HttpRequestException>()
+            .Or<TaskCanceledException>() // network stall
+            .OrResult(r => (int)r.StatusCode >= 500 || r.StatusCode == HttpStatusCode.TooManyRequests)
+            .WaitAndRetryAsync(retryCount: 3,
+                retry => TimeSpan.FromSeconds(Math.Pow(x: 2, retry)) +
+                         TimeSpan.FromMilliseconds(RandomNumberGenerator.GetInt32(fromInclusive: 100,
+                             toExclusive: 500)));
+
+    private static AsyncPolicy<HttpResponseMessage> GetRateLimitPolicy()
+        => Policy.RateLimitAsync<HttpResponseMessage>(numberOfExecutions: 10, TimeSpan.FromSeconds(seconds: 1));
+
+    private static SocketsHttpHandler CreateScrapingHandler(bool allowAutoRedirect)
+        => new()
+        {
+            AutomaticDecompression = DecompressionMethods.All,
+            PooledConnectionLifetime = TimeSpan.FromMinutes(minutes: 10),
+            PooledConnectionIdleTimeout = TimeSpan.FromMinutes(minutes: 2),
+            MaxConnectionsPerServer = 50,
+            ConnectTimeout = TimeSpan.FromSeconds(seconds: 15),
+            KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always,
+            KeepAlivePingDelay = TimeSpan.FromSeconds(seconds: 30),
+            KeepAlivePingTimeout = TimeSpan.FromSeconds(seconds: 10),
+            UseCookies = false,
+            AllowAutoRedirect = allowAutoRedirect
+        };
 }
