e6a71343b9655809adc12957a546b7070f6b3df6
VahidN <vahid_nasiri@yahoo.com>
2026-05-01 11:11:34 +0330

    Add MarkdownTableGenerator

---
+++ src/DNTCommon.Web.Core/HtmlUtils/MarkdownTableGenerator.cs
@@ -0,0 +1,65 @@
+using System.Text;
+
+namespace DNTCommon.Web.Core;
+
+/// <summary>
+///     تولید جدول Markdown از روی سرستون‌ها و ردیف‌ها
+/// </summary>
+public static class MarkdownTableGenerator
+{
+    /// <summary>
+    ///     تولید جدول Markdown از روی سرستون‌ها و ردیف‌ها
+    /// </summary>
+    /// <param name="headers">لیست نام ستون‌ها</param>
+    /// <param name="rows">لیست ردیف‌ها که هر ردیف لیستی از مقادیر است</param>
+    /// <returns>رشته‌ی جدول در قالب Markdown</returns>
+    public static string GenerateMarkdownTable(this ICollection<string>? headers,
+        ICollection<ICollection<string>>? rows)
+    {
+        var columnCount = headers?.Count ?? 0;
+
+        if (headers is null || columnCount == 0)
+        {
+            return string.Empty;
+        }
+
+        var markdownTable = new StringBuilder();
+
+        // خط سرستون‌ها
+        markdownTable.Append(value: '|');
+        markdownTable.AppendJoin(separator: '|', headers.Select(EscapeMarkdownTableCell));
+        markdownTable.AppendLine(value: "|");
+
+        // خط جداکننده (alignment پیش‌فرض چپ)
+        markdownTable.Append(value: '|');
+        markdownTable.AppendJoin(separator: '|', headers.Select(_ => "---"));
+        markdownTable.AppendLine(value: "|");
+
+        // ردیف‌های داده
+        if (rows is null || rows.Count == 0)
+        {
+            return string.Empty;
+        }
+
+        foreach (var row in rows)
+        {
+            var rowCells = row.Take(columnCount).ToList(); // تعداد ستون‌ها را برابر با هدر می‌کنیم
+
+            if (rowCells.Count < columnCount)
+            {
+                // پر کردن سلول‌های خالی در صورت کمبود
+                rowCells.AddRange(Enumerable.Repeat(string.Empty, columnCount - rowCells.Count));
+            }
+
+            markdownTable.Append(value: '|');
+            markdownTable.AppendJoin(separator: '|', rowCells.Select(EscapeMarkdownTableCell));
+            markdownTable.AppendLine(value: "|");
+        }
+
+        return markdownTable.ToString();
+    }
+
+    public static string EscapeMarkdownTableCell(this string? cell)
+        => cell?.Replace(oldValue: "|", newValue: "\\|", StringComparison.Ordinal)
+            .Replace(oldValue: "\n", newValue: "<br>", StringComparison.Ordinal) ?? string.Empty;
+}
