bf8007048914a1bb9cd97418a1bdd127c5ca264e
VahidN <vahid_nasiri@yahoo.com>
2026-05-14 12:46:07 +0330

    Update dependencies

--- update-dependencies.cmd
+++ update-dependencies.cmd
@@ -1,4 +1,6 @@
 dotnet restore --ignore-failed-sources -v diag
 dotnet list package --outdated
+rem dotnet restore --ignore-failed-sources -v diag --source https://mirror-nuget.runflare.com/v3/index.json
+rem dotnet list package --outdated --source https://mirror-nuget.runflare.com/v3/index.json
 dotnet restore --ignore-failed-sources -v diag
 pause
